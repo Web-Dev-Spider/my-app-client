ffcef8571f0fc14fd4f3416040d69bddac27c108
Syam <nmsyam.mohan@gmail.com>
2026-01-29 14:30:06 +0530

    axios call corrected with credentials obj

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
         e.preventDefault()
 
         try {
-            const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/user/login`, loginData)
+            const res = await axios.post(`${import.meta.env.VITE_BASE_URL}/user/login`, loginData, { withCredentials: true })
             if (res.data.success) {
 
                 navigate('/home')
